ae029512078d4a7331acfae0d2f4f0597922cf0f
shaiful <msacbd@gmail.com>
2023-03-08 13:54:28 -0700

    code and results for change distribution of top x% methods.

--- code/rq1/change_distribution.py
+++ code/rq1/change_distribution.py
@@ -18,7 +18,7 @@ marks = ["^", "d", "o", "v", "p", "s", "<", ">"]
 width = [3, 3, 3, 3, 3, 3, 3, 3]
 marks_size = [20, 20, 20, 20, 20, 10, 12, 15]
 marker_color = ['#0F52BA', '#ff7518', '#6CA939', '#e34234', '#756bb1', 'brown', '#c994c7', '#636363']
-gaps = [4,4, 4, 4, 4, 4, 4, 4]
+gaps = [4, 4, 4, 4, 4, 4, 4, 4]
 
 
 def list_projects():
@@ -51,6 +51,7 @@ def process(project):
     feature_indexes = build_indexes(line)
     lines = fr.readlines()
     total_change = 0
+    c = 0
     for line in lines:
         method, value, total_change = process_method(line, feature_indexes, total_change)
         if method == 'invalid':
@@ -64,16 +65,17 @@ def process_method(line, feature_indexes, total_change):
     values = line.split("\t")
     method = values[feature_indexes['file']].strip()
     age = int(values[0])
+    if apply_age_restriction == 1 and age < age_restriction:
+        return 'invalid', 0, total_change
+
     diffs = values[feature_indexes['DiffSizes']]
     adds = values[feature_indexes['NewAdditions']]
     edits = values[feature_indexes['EditDistances']]
+    bugs = values[feature_indexes['RiskyCommit']]
     change_ages = values[feature_indexes['ChangeAtMethodAge']].split(",")
-    values = decide_type(diffs, adds, edits)
+    values = decide_type(diffs, adds, edits, bugs)
     value = calculate_value(values, change_ages)
     total_change = total_change + value
-
-    if apply_age_restriction == 1 and age < age_restriction:
-        method = 'invalid'
     return method, value, total_change
 
 
@@ -83,7 +85,7 @@ def calculate_value(values, change_ages):
     for i in range(1, len(values)):
         if int(change_ages[i]) > age_restriction and apply_age_restriction == 1:
             break
-        if change_type == 'revision':
+        if change_type == 'revision' or change_type == 'bugs':
             if int(values[i]) > 0:
                 value = value + 1
         else:
@@ -92,7 +94,7 @@ def calculate_value(values, change_ages):
     return value
 
 
-def decide_type(diffs, adds, edits):
+def decide_type(diffs, adds, edits, bugs):
     if change_type == 'revision' or change_type == 'diffs':
         return diffs
 
@@ -102,34 +104,32 @@ def decide_type(diffs, adds, edits):
     if change_type == 'edits':
         return edits
 
+    if change_type == 'bugs':
+        return bugs
+
 
 def analyse(project, methods, total_change):
     index = 0
     stats = {}
     methods = sorted(methods.items(), key=lambda item: item[1], reverse=True)
-    # for method in methods
-    # print (methods)
-    # print (methods)
-    # print(project)
-    count = 1.0
+    count_methods = 1.0
     total_methods = len(methods)
     moving_change = 0.0
-    min = 1000000000
-    max = -1
-    name = ''
     for method in methods:
-        if method[1] > max:
-            max = method[1]
-            name = method[0]
-        if method[1] < min:
-            min = method[1]
-        moving_change += method[1]
-        current_percent_methods = float(count / total_methods)
+       # if method[1] > max:
+       #     max = method[1]
+       # if method[1] < min:
+       #     min = method[1]
+        moving_change += float(method[1])
+        current_percent_methods = float(count_methods / total_methods)
         percent_change = float(moving_change / total_change)
-        count += 1
+        #print(count_methods, total_methods, current_percent_methods, moving_change, total_change, percent_change)
+        count_methods += 1
+
         if current_percent_methods * 100 >= given_percent_methods[index]:
             stats[given_percent_methods[index]] = percent_change
             index = index + 1
+
             if index == len(given_percent_methods):
                 break
 
@@ -150,11 +150,12 @@ def draw_graph(STATS):
         for project in STATS:
             a.append(STATS[project][method_percent])
         X, Y = ecdf(a)
-        ln = (plt.plot(X*100, Y))
+        ln = (plt.plot(X * 100, Y))
         plt.setp(ln, linewidth=width[index], ls=styles[index], marker=marks[index], markersize=marks_size[index],
                  color=marker_color[index], markevery=gaps[index])
         index += 1
 
+        
     plt.xlabel("Coverage", fontsize=24)
     plt.ylabel("CDF", fontsize=22)
     # plt.xscale("log")
@@ -172,20 +173,18 @@ def draw_graph(STATS):
 
 if __name__ == "__main__":
     global given_percent_methods
-    STATS = {}
-    # TODO we did not use age restriction yet
+    CDF_STATS = {}
     global age_restriction
     global change_type
-    change_types = ['revision', 'adds', 'diffs', 'edits']
+    change_types = ['revision', 'adds', 'diffs', 'edits', 'bugs']
     given_percent_methods = [5, 10, 15, 20]
-
-    change_type = change_types[0]
-    apply_age_restriction = 0
+    change_type = change_types[4]
+    apply_age_restriction = 1
     age_restriction = 730
     projects = list_projects()
 
     for project in projects:
         methods, total_change = process(project)
-        STATS[project] = analyse(project, methods, total_change)
+        CDF_STATS[project] = analyse(project, methods, total_change)
 
-    draw_graph(STATS)
+    draw_graph(CDF_STATS)
